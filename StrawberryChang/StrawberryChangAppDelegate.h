c49543e8fdb17b66ba2a74667829aaacf0c302f3
Xiaochun Shen <snowdonshen@gmail.com>
2011-09-06 08:59:58 +0800

    Initial Commit

---
+++ StrawberryChang/StrawberryChangAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  StrawberryChangAppDelegate.h
+//  StrawberryChang
+//
+//  Created by  Chuns on 11-9-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StrawberryChangAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
