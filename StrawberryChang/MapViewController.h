bb3b9626a331cab8a65e50baf4409a1f68981dab
Xiaochun Shen <snowdonshen@gmail.com>
2011-09-09 17:56:43 +0800

    add map image

---
+++ StrawberryChang/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  StrawberryChang
+//
+//  Created by  Chuns on 11-9-9.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MapViewController : UIViewController {
+    
+}
+
+@end
