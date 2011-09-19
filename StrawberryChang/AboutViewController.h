b7bf9bab066ece146a00132916ba84f05ae10687
Xiaochun Shen <snowdonshen@gmail.com>
2011-09-19 11:53:30 +0800

    add about view controller

---
+++ StrawberryChang/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  StrawberryChang
+//
+//  Created by  Chuns on 11-9-19.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutViewController : UIViewController {
+    
+}
+
+@end
